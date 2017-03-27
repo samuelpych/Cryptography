b20c107f9f950113b9dda0a30021e5f63dc067c0
samuelpych <samuelpych@hanovernorwichschools.org>
2017-03-27 14:37:56 -0400

    Updated from Brython Server: 3/27/2017 2:37:56 PM

--- cryptography.py
+++ cryptography.py
@@ -1,6 +1,6 @@
 """
 cryptography.py
-Author: <your name here>
+Author: Sam
 Credit: <list sources used, if any>
 
 Assignment:
@@ -8,5 +8,11 @@ Assignment:
 Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+To get a character's numeric representation, find the index of that character in the string 
+(use the code associations.find(char) to get the index of char's first occurrence in associations)
+To get a character back from this numeric representation, get the character at that number's index 
+in the string (using associations[index]).
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+associations.find(char)
+using associations[index]
