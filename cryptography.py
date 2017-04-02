3740ebcbe5498520360dc17148903cb247d21dff
samuelpych <samuelpych@hanovernorwichschools.org>
2017-04-02 11:39:57 -0400

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -14,36 +14,66 @@ To get a character back from this numeric representation, get the character at t
 in the string (using associations[index]).
 MUST ENCRYPT AND DECRYPT 
 """
+"""
+cryptography.py
+Author: Sam
+Credit: none yet
+
+Assignment:
+
+Write and submit a program that encrypts and decrypts user data.
+
+See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+To get a character's numeric representation, find the index of that character in the string 
+(use the code associations.find(char) to get the index of char's first occurrence in associations)
+To get a character back from this numeric representation, get the character at that number's index 
+in the string (using associations[index]).
+MUST ENCRYPT AND DECRYPT 
+"""
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-key=input("Key: ")
-letter=input("Enter e to encrypt, d to decrypt, or q to quit: ")
-instruction=input("Message: ")
-def encrypt(message, key, process):
-     messnum = [associations.find(x) for x in message]
-     keynum = [associations.find(x) for x in key]
-     keynum = keynum*(len(messnum)//len(keynum))+keynum[:len(messnum)%len(keynum)]
-     keynum = [process*x for x in keynum]
-     emess = [sum(x) for x in zip(messnum, keynum)]
-     emess = ''.join([associations[x%len(associations)] for x in emess])
-     print(emess)
-      
-process = "a"
-process = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-        print("Goodbye!")
-while process != "q":
-    if process == "e":
-        message = input("Message: ")
-        key = input("Key: ")
-        encrypt(message, key, 1)
-    elif process == "d":
-        message = input("Message: ")
-        key = input("Key: ")
-        encrypt(message, key, -1)
+repeatasoc="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+order=str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+while order  not in ["q"]:
+    if order =="e":
+        one=input("Message: ")
+        message=list(one)
+        decode=input("Key: ")
+        key= list(decode)
+        for a in range(0, len(message)):
+            message[a]=associations.find(message[a])
+        for a in range(0, len(key)):
+            key[a]=associations.find(key[a])
+        while len(key)<len(message):
+            key.extend(key)
+        ab=[a+b for a,b in zip(message, key)]
+        for a in range(0, len(message)):
+            thingy=repeatasoc[ab[a]]
+            print(thingy,end="")
+        print( )
+    if order=="d":
+        listmsg=input("Message: ")
+        message=list(listmsg)
+        key1=input("Key: ")
+        key= list(key1)
+        for a in range(0, len(message)):
+            message[a]=associations.find(message[a])
+        for a in range(0, len(key)):
+            key[a]=associations.find(key[a])
+        while len(key)<len(message):
+            key.extend(key)
+        ab=[a-b for a,b in zip(message, key)]
+        for a in range(0, len(message)):
+            thingy=repeatasoc[ab[a]]
+            print(thingy,end="")
+        print( )
     else:
         print("Did not understand command, try again.")
-if process == "q":
+    order=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+if order== "q":
         print("Goodbye!")
-associations[index]
+        
+        
+
 """
 must convert characters to numbers and back before actual math
 """
